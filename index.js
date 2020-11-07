9d6dae73afeacf9d46923bb081a9a12e45305880
Estók Ádám <Estoka07@outlook.com>
2020-11-07 18:52:57 +0000

    added stuff

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const cmdhandler = require('wokcommands');
 const config = require('./config.json');
+const command = require('./src/handlers/command');
+
+/*this is for for always on, don't worry if
+you will run this on your pc, or on a vps.*/
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hey Bud!'));
 
-app.listen(port, () => 
-console.log(`Your App is listening at https.//localhost:${port}`)
+app.listen(port, () =>
+	console.log(`Your App is listening at https.//localhost:${port}`)
 );
 
+//after this is is the normal code.
+
 client.on('ready', () => {
-	console.log('The Client Is On!');
+	console.log('Ready!');
+
+	command(client, (message) => {
+		message.channel.send('Pong!');
+	})
 })
 
 client.login(process.env.TOKEN);
